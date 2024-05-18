ec2b482fa689e2cb176dbbc97005d1ccdab1713a
tayyabaawan642 <157307322+tayyabaawan642@users.noreply.github.com>
2024-05-18 21:33:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let age = 24;
+alert("I'm " + age + " years old.");
+
+let count = 5;
+alert("You have visited this site " + count + " times.");
+
+let birthYear = 1999;
+document.write("My birth year is " + birthYear + "\n" + " Data type of my declared variable is number.");
---
+++ app2.js
@@ -0,0 +1,5 @@
+let visitorName = prompt("Enter Your Name.");
+let productTitle = prompt("Enter Product Title.");
+let qty = prompt("How many products you wants to order?");
+
+document.write(visitorName + " ordered " + qty + " " + productTitle + " from www.xyzClothing.com");
